6c87eea91ddcb812322a01edc72581673c21f95c
Aditya7898 <aditya.cs.ind@gmail.com>
2018-09-27 12:27:56 +0530

    change port according to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+// const port = process.env.port || 3000;
 
 var app = express();
 
@@ -68,6 +68,6 @@ app.get('/projects', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is up on port ${port}`);
 });
